ac848681b51096d5e4b135e1973dcee80fe5ba58
alistair-barfoot <104738937+alistair-barfoot@users.noreply.github.com>
2025-07-17 15:02:44 -0400

    Added in power, average power, and max power

--- csv_insights.c
+++ csv_insights.c
@@ -6,7 +6,9 @@
 #define FALSE 0
 #define TRUE 1
 #define VOLT_MAX 5000
+#define MIN_POWER 0
 #define VOLT_DROP_THRESHOLD 4900
+#define MAX_ROWS 10000
 
 int main(int argc, char *argv[])
 {
@@ -15,12 +17,14 @@ int main(int argc, char *argv[])
     printf("Incorrect number of arguments\n");
     return -1;
   }
-  int voltage_vals[10000];
+  int voltage_vals[MAX_ROWS];
   for (int i = 0; i < argc; i++)
   {
     printf("%s\n", argv[i]);
   }
   int start = atoi(argv[2]), end = atoi(argv[3]);
+  int num_periods = end - start;
+  double time_elapsed = num_periods * TIME_PERIOD_MS / 1000.0;
   FILE *fp = fopen(argv[1], "r");
   printf("%s\n", argv[1]);
 
@@ -42,10 +46,12 @@ int main(int argc, char *argv[])
   int voltage, drop_min;
   int in_drop = FALSE;
   int drop_start = 0;
-  double current;
+  double current, power;
   int lowest_drop = VOLT_MAX;
+  double max_power = MIN_POWER;
 
   int total_drop_length = 0, drop_count = 0, total_drop_min = 0;
+  double total_power = 0;
 
   for (int i = start; i < end; i++)
   {
@@ -67,13 +73,17 @@ int main(int argc, char *argv[])
     // printf("%s\n", current_str);
     voltage = atoi(voltage_str);
     current = atof(current_str);
-    printf("I=%.2lf\n", current);
+    power = voltage * current / 1000; // W
+    total_power += power;
+    if (power > max_power)
+      max_power = power;
+    printf("I=%03.0lfmA, P=%04.0lfW\n", current, power);
     if (voltage < VOLT_DROP_THRESHOLD && !in_drop)
     {
       drop_start = i;
       drop_min = VOLT_MAX;
       in_drop = TRUE;
-      printf("Index: %d, Voltage: %d\n", i, voltage);
+      printf("Start voltage drop. Index: %d, Voltage: %d\n", i, voltage);
     }
     if (in_drop && voltage < drop_min)
     {
@@ -93,8 +103,11 @@ int main(int argc, char *argv[])
     }
   }
 
+  printf("This trial lasted for %.0lfs.\n", time_elapsed);
   printf("The voltage dropped %d times and the average drop length was %.1lfs.\n", drop_count, total_drop_length / (double)drop_count / 1000);
   printf("The average drop fell to %.0lfV.\n", total_drop_min / (double)drop_count);
   printf("The lowest the voltage dropped was %dV.\n", lowest_drop);
+  printf("The average power consumption during this trial was %.1lfW.\n", total_power / (float)num_periods);
+  printf("The maximum power reached during this trial was %.1lfW", max_power);
   return 0;
 }
